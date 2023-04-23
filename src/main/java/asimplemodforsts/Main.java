f8481523179435c49bb10934fb66e0322308c909
L_Misaki <2481784450@qq.com>
2023-04-23 17:46:02 +0800

    hello world

--- src/main/java/asimplemodforsts/Main.java
+++ src/main/java/asimplemodforsts/Main.java
@@ -5,7 +5,7 @@ import com.evacipated.cardcrawl.modthespire.lib.SpireInitializer;
 @SpireInitializer
 public class Main {
     public Main(){
-
+        System.out.println("hello world");
     }
     public static void initialize(){
         new Main();
