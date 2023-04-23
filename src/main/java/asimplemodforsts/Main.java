e0f9a4b2254c94645cadd48da8bc60755328db9a
L_Misaki <2481784450@qq.com>
2023-04-23 21:32:38 +0800

    新遗物

--- src/main/java/asimplemodforsts/Main.java
+++ src/main/java/asimplemodforsts/Main.java
@@ -1,13 +1,19 @@
 package asimplemodforsts;
 
+import asimplemodforsts.relics.HolyGrail;
+import basemod.BaseMod;
+import basemod.helpers.RelicType;
 import com.evacipated.cardcrawl.modthespire.lib.SpireInitializer;
 
 @SpireInitializer
 public class Main {
     public Main(){
-        System.out.println("hello world");
+
     }
     public static void initialize(){
         new Main();
+    }
+    public void receiveEditRelics(){
+        BaseMod.addRelic(new HolyGrail(), RelicType.SHARED);
     }
 }
---
+++ src/main/java/asimplemodforsts/relics/HolyGrail.java
@@ -0,0 +1,30 @@
+package asimplemodforsts.relics;
+
+import basemod.abstracts.CustomRelic;
+import com.badlogic.gdx.graphics.Texture;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DrawCardAction;
+import com.megacrit.cardcrawl.actions.common.GainEnergyAction;
+import com.megacrit.cardcrawl.actions.common.RelicAboveCreatureAction;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.relics.AbstractRelic;
+import com.megacrit.cardcrawl.relics.RedSkull;
+
+public class HolyGrail extends CustomRelic {
+    public static final String ID="holy grail";
+
+    public HolyGrail() {
+        super(ID, "holy_grail.png", AbstractRelic.RelicTier.RARE, AbstractRelic.LandingSound.SOLID);
+    }
+
+    public void atTurnStart(){
+        flash();
+        addToBot((AbstractGameAction)new RelicAboveCreatureAction((AbstractCreature) AbstractDungeon.player, this));
+        addToBot((AbstractGameAction)new GainEnergyAction(1));
+    }
+
+    public AbstractRelic makeCopy() {
+        return new HolyGrail();
+    }
+}
