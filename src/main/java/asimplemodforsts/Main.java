0914c8766ff734b4867146aa5f5320f408ca0985
Hotaru-Snowy <hotarusnowy@hotmail.com>
2023-04-23 16:44:26 +0800

    创建基础类并忽略.idea文件夹

---
+++ src/main/java/asimplemodforsts/Main.java
@@ -0,0 +1,13 @@
+package asimplemodforsts;
+
+import com.evacipated.cardcrawl.modthespire.lib.SpireInitializer;
+
+@SpireInitializer
+public class Main {
+    public Main(){
+
+    }
+    public static void initialize(){
+        new Main();
+    }
+}
