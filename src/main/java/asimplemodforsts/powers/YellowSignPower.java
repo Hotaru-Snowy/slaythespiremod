430d8fa0e22ea36f91020f74da3d73d33966510c
Hotaru-Snowy <hotarusnowy@hotmail.com>
2023-07-16 21:23:38 +0800

    增加黄色蓝色信号BUFF(虽然还没有相应的卡)，更新三色球的图标。

---
+++ src/main/java/asimplemodforsts/powers/YellowSignPower.java
@@ -0,0 +1,33 @@
+package asimplemodforsts.powers;
+
+import asimplemodforsts.ResourceLib;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.helpers.ImageMaster;
+import com.megacrit.cardcrawl.localization.PowerStrings;
+
+public class YellowSignPower extends SignBallPower {
+    public static final String POWER_ID = "YellowSignPower";
+    private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
+    public static final String NAME = powerStrings.NAME;
+    public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
+    public YellowSignPower(AbstractCreature o, int n){
+        name=NAME;
+        ID=POWER_ID;
+        owner=o;
+        amount=n;
+        region48 = new TextureAtlas.AtlasRegion(ImageMaster.loadImage(ResourceLib.powerImagePath(POWER_ID)), 0, 0, 32, 32);
+        region128 = new TextureAtlas.AtlasRegion(ImageMaster.loadImage(ResourceLib.powerImageLargePath(POWER_ID)), 0, 0, 64, 64);
+        updateDescription();
+    }
+    @Override
+    public void stackPower(int stackAmount) {
+        this.fontScale = 8.0F;
+        this.amount += stackAmount;
+    }
+    @Override
+    public void updateDescription() {
+        this.description = DESCRIPTIONS[0] + this.amount + DESCRIPTIONS[1];
+    }
+}
